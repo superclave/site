0d2b3ad7da7eeefaa704f50716cd8396038d6523
Tom Snelling <tomsnelling8@gmail.com>
2025-07-17 17:46:08 +0100

    github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://superclave.github.io',
+  base: 'site'
+});
